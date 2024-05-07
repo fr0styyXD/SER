16f74e822b70cd6a3b966decf26f419d085fd917
Aksh <53231153+fr0styyXD@users.noreply.github.com>
2024-05-07 13:44:20 +0530

    new.py

--- new.py
+++ new.py
@@ -17,11 +17,11 @@ def main():
         st.sidebar.success('To try by yourself by adding a audio file .')
         application()
     if selected_box=='view source code':
-        st.code(get_file_content_as_string("app.py"))
+        st.code(get_file_content_as_string("new.py"))
 
 @st.cache(show_spinner=False)
 def get_file_content_as_string(path):
-    url = 'https://raw.githubusercontent.com/chiluveri-sanjay/Emotion-recognition/main/' + path
+    url = 'https://raw.githubusercontent.com/fr0styyXD/SER/main/' + path
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
     
